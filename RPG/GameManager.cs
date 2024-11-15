40aa8959412b1edc07b39a7aa22375e28bbf34f5
김진호 <russiakim2008@gmail.com>
2024-11-15 22:48:29 +0900

    Start Project
    Start Project

---
+++ RPG/GameManager.cs
@@ -0,0 +1,44 @@
+﻿namespace RPG
+{
+    class GameManager
+    {
+        Map map = new Map();
+
+
+        public void Update()
+        {
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+
+                for (int i = 0; i < map._map2.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map._map2.GetLength(1); j++)
+                    {
+                        Console.Write(map._map2[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                DelayerTime();
+
+                Console.SetCursorPosition(0, 0);
+                //Console.Clear();
+                for (int i = 0; i < map._map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map._map.GetLength(1); j++)
+                    {
+                        Console.Write(map._map[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                DelayerTime();
+            }
+        }
+
+        public void DelayerTime()
+        {
+            Thread.Sleep(2000);
+        }
+
+    }
+}
---
+++ RPG/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    class Program
+    {
+        static GameManager _gameManager = new GameManager();
+
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = 85; // 창 너비 설정
+            Console.WindowHeight = 40; // 창 높이 설정
+
+            _gameManager.Update();
+        }
+    }
+}
